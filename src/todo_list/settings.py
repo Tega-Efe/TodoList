210770044d294bf0a7a6ab0836d224e6a009f88d
Tega-Efe <dominicadjeh@gmail.com>
2024-09-12 11:14:12 +0100

    backup

--- src/todo_list/settings.py
+++ src/todo_list/settings.py
@@ -81,22 +81,22 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD' : 'JZBmPdgTammAUJLBhqdundwuSxFeeyBd',
-        'HOST':'roundhouse.proxy.rlwy.net' ,
-        'PORT' : '52334',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD' : 'JZBmPdgTammAUJLBhqdundwuSxFeeyBd',
+#         'HOST':'roundhouse.proxy.rlwy.net' ,
+#         'PORT' : '52334',
+#     }
+# }
 
 
 # Password validation
